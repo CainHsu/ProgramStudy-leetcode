c0c8b3a41c33cebba253328c55f42386bf7cfd54
CainHsu <xu.chen28@qq.com>
2019-12-08 15:49:48 +0800

    Daily update
    - add file 150.hpp
    - add file 739.md
    - finished  150.hpp
    - update REAMME.md
    - update stack.hpp (150.hpp)

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # Program Study / Leetcode
 [![Travis](https://img.shields.io/badge/language-C++-green.svg)]() 
 
+## 介绍
 本项目旨在自我编程能力的提高以及leetcode题解代码和思路的记录
 
 按计划，我会每日更新一个题解
+
+## 题解分类
+### 堆栈
+[20.Valid-Parentheses](./ProblemsSolved/20.Valid-Parentheses.md)
+
+## 待完成
+思路总结：
+- [739.Daily Temperatures](./ProblemsSolved/739.Daily-Temperatures.md)
+- [150.Evaluate Reverse Polish Notation](./ProblemsSolved/150.Evaluate-Reverse-Polish-Notation.md)
+
+代码编写：
+
 
 
--- leetCode/150.Evaluate Reverse Polish Notation.hpp
+++ leetCode/150.Evaluate Reverse Polish Notation.hpp
@@ -1,54 +1,15 @@
 //
-// Created by CainHsu on 2019/12/6.
+// Created by xuche on 2019/12/8.
 //
 
-#ifndef TEST_STACK_HPP
-#define TEST_STACK_HPP
+#ifndef PROGRAMSTUDY_LEETCODE_150_EVALUATE_REVERSE_POLISH_NOTATION_HPP
+#define PROGRAMSTUDY_LEETCODE_150_EVALUATE_REVERSE_POLISH_NOTATION_HPP
 
+#include "vector"
 #include "string"
 #include "stack"
 
 using namespace std;
-
-bool isValid(string s) {
-
-    if(s.empty()) return true;
-    if(s.length()&1) return false;
-    stack<char> temp;
-
-    for(auto i:s){
-        if(i == '[' || i == '(' || i == '{') temp.push(i);
-
-        if((i == '}' || i == ')' || i == ']') && temp.empty()) {
-            return false;
-        }
-
-        if((i == ')' && temp.top() == '(') || (i == ']' && temp.top() == '[') || (i == '}' && temp.top() == '{')) temp.pop();
-    }
-    return temp.empty();
-
-}
-
-
-vector<int> dailyTemperatures(vector<int>& T) {
-    vector<int> ans;
-    if(T.empty()) return ans;
-    ans.resize(T.size());
-    stack<int> index;
-    for(int i = 0; i < T.size(); ++i){
-        while ((!index.empty()) && T[i] > T[index.top()]) {
-            ans[index.top()] = i - index.top();
-            index.pop();
-        }
-        index.push(i);
-    }
-    while(!index.empty()){
-        ans[index.top()] = 0;
-        index.pop();
-    }
-    return ans;
-}
-
 
 int evalRPN(vector<string>& tokens) {
     int res = 0;
@@ -89,4 +50,4 @@ int evalRPN(vector<string>& tokens) {
     return stoi(temp.top());
 }
 
-#endif //TEST_STACK_HPP
+#endif //PROGRAMSTUDY_LEETCODE_150_EVALUATE_REVERSE_POLISH_NOTATION_HPP
